1be9ed58df2f2377320231ca0a5cb86eff284008
dwidge <dwidge+gh@gmail.com>
2024-08-20 16:42:23 +0200

    Replace FilterTags styles with css file

--- src/FilterTags.tsx
+++ src/FilterTags.tsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import './FilterTags.css';
+
 interface FilterTagsProps {
   filters: { [column: string]: string[] };
   setFilters: React.Dispatch<
@@ -17,52 +20,17 @@ export const FilterTags: React.FC<FilterTagsProps> = ({
   };
 
   return (
-    <div
-      className="filter-tags"
-      style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(auto-fill, minmax(15em, 1fr))",
-        gap: ".5em",
-      }}
-    >
+    <div className="filter-tags">
       {Object.entries(filters).flatMap(([column, columnFilters]) =>
         columnFilters.map((filter, index) => (
           <button
             key={`${column}-${index}`}
             className="filter-tag"
-            style={{
-              padding: 0,
-              color: "white",
-              backgroundColor: "transparent",
-              borderRadius: "5px",
-              borderColor: "darkred",
-              borderWidth: 2,
-              flex: 1,
-              display: "flex",
-              gap: ".5em",
-            }}
             onClick={() => removeFilter(column, filter)}
             aria-label={`Remove filter ${filter} from ${column}`}
           >
-            <div
-              style={{
-                fontSize: "0.9em",
-                fontStyle: "italic",
-                padding: ".3em .5em",
-                backgroundColor: "darkred",
-              }}
-            >
-              {column}
-            </div>
-            <div
-              style={{
-                flex: 1,
-                fontSize: "0.9em",
-                padding: ".3em .5em",
-              }}
-            >
-              {filter}
-            </div>
+            <div className="filter-tag-column">{column}</div>
+            <div className="filter-tag-filter">{filter}</div>
           </button>
         ))
       )}
